5d700d4d1c41ec0f1e2c48f885c407b352672d86
Sokty Heng <sokty@Soktys-MacBook-Pro.local>
2025-11-15 18:00:28 +0700

    fixing credential api

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,29 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, OAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7jFdZ6JU1e9Nn3I2weCfnySp8NJcvmdw",
-  authDomain: "skillsync-11e17.firebaseapp.com",
-  projectId: "skillsync-11e17",
-  storageBucket: "skillsync-11e17.firebasestorage.app",
-  messagingSenderId: "230295884777",
-  appId: "1:230295884777:web:02943569582b33d9a13178"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication
 export const auth = getAuth(app);
-
-// Configure Microsoft OAuth Provider
 export const microsoftProvider = new OAuthProvider('microsoft.com');
 
-// Optional: Configure Microsoft provider settings
 microsoftProvider.setCustomParameters({
-  tenant: 'common', // or your tenant ID
+  tenant: 'common',
 });
 
 export default app;
